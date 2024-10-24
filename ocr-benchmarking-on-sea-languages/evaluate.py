1f9a7350352308140eb612996d4f50eda5d07402
jasonqiu212 <jason_qiu@hotmail.com>
2024-10-25 01:25:21 +0800

    feat: sort evaluation results in csv

--- ocr-benchmarking-on-sea-languages/evaluate.py
+++ ocr-benchmarking-on-sea-languages/evaluate.py
@@ -8,8 +8,9 @@ import jiwer
 
 def evaluate_to_csv(source_path: str, target_file_name: str, prediction_file_name: str, output_file_name: str):
     data = [['Article Name', 'CER', 'WER']]
+    sorted_articles = sorted(os.listdir(source_path))
 
-    for article in os.listdir(source_path):
+    for article in sorted_articles:
         target_file_path = f'{source_path}/{article}/{target_file_name}'
         prediction_file_path = f'{
             source_path}/{article}/{prediction_file_name}'
@@ -25,7 +26,7 @@ def evaluate_to_csv(source_path: str, target_file_name: str, prediction_file_nam
             formatted_wer = round(wer, 4)
             data.append([article, formatted_cer, formatted_wer])
         else:
-            data.append([article, -1, -1])
+            print(f'{article} does not have results')
 
     with open(f'{source_path}/{output_file_name}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
