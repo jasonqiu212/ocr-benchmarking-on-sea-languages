02612e8bac0dcf27fe12d08db125248ede0cd611
jasonqiu212 <jason_qiu@hotmail.com>
2024-10-24 19:01:32 +0800

    refactor: refactor out ocr methods

--- ocr-benchmarking-on-sea-languages/ocr/tesseract.py
+++ ocr-benchmarking-on-sea-languages/ocr/tesseract.py
@@ -1,77 +1,8 @@
-"""This file provides operations to run different OCR tools."""
-
 import os
 import time
 from typing import List
 
 import pytesseract
-
-
-def run_easy_ocr_on_all(source_path: str, reader):
-    """
-    Runs EasyOCR on all articles in a directory and stores results in an output file.
-
-    Args:
-        source_path: Path to articles
-        reader: EasyOCR reader that specifies languages to recognize
-    """
-    for f in os.listdir(source_path):
-        start_time = time.time()
-        print(f'Running on article: {f}')
-
-        result_file_path = f'{source_path}/{f}/easy-ocr-results.txt'
-        if os.path.exists(result_file_path):
-            print('Results exist')
-            continue
-
-        res = ''
-        i = 0
-        image_file_path = f'{source_path}/{f}/page-{i}.png'
-        while os.path.exists(image_file_path):
-            text = reader.readtext(image_file_path, detail=0)
-            res += ' '.join(text)
-            i += 1
-            image_file_path = f'{source_path}/{f}/page-{i}.png'
-        with open(result_file_path, 'wt') as outfile:
-            outfile.write(res)
-
-        end_time = time.time()
-        time_taken = end_time - start_time
-        print(f'Finished! Time taken: {time_taken} seconds')
-
-
-def run_easy_ocr(articles: List[str], source_path: str, reader):
-    """
-    Runs EasyOCR on selected articles in a directory and stores results in an output file.
-
-    Args:
-        articles: List of articles to perform EasyOCR on
-        source_path: Path to articles
-        reader: EasyOCR reader that specifies languages to recognize
-    """
-    for article in articles:
-        start_time = time.time()
-        print(f'Running on article: {article}')
-
-        result_file_path = f'{source_path}/{article}/easy-ocr-results.txt'
-        if os.path.exists(result_file_path):
-            print('Results exist')
-            continue
-
-        res = ''
-        i = 0
-        image_file_path = f'{source_path}/{article}/page-{i}.png'
-        while os.path.exists(image_file_path):
-            text = reader.readtext(image_file_path, detail=0)
-            res += ' '.join(text)
-            i += 1
-            image_file_path = f'{source_path}/{article}/page-{i}.png'
-        with open(result_file_path, 'wt') as outfile:
-            outfile.write(res)
-
-        end_time = time.time()
-        time_taken = end_time - start_time
-        print(f'Finished! Time taken: {time_taken} seconds')
 
 
 def run_tesseract_on_all(source_path: str, language: str):
