f7370d19e855668735b8dc68338ab049d9c13791
jasonqiu212 <jason_qiu@hotmail.com>
2024-12-28 10:32:35 +0800

    chore: clean up server scripts

--- ocr-benchmarking-on-sea-languages/run_tesseract.py
+++
@@ -1,6 +0,0 @@
-from ocr.tesseract import run_tesseract_on_all
-
-run_tesseract_on_all('../data/thai', 'tha')
-run_tesseract_on_all('../data/english', 'eng')
-run_tesseract_on_all('../data/vietnamese', 'vie')
-run_tesseract_on_all('../data/bahasa', 'ind')
--- ocr-benchmarking-on-sea-languages/run_tesseract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH --time=999
-#SBATCH --job-name=run_tesseract
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=jason.qiu@u.nus.edu
-#SBATCH --gpus=1
-
-python3 run_tesseract.py
