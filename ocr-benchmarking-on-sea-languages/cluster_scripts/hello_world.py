75569f4a54fbd555916c79f71da29865a9906431
jasonqiu212 <jason_qiu@hotmail.com>
2024-12-28 17:52:37 +0800

    feat: add sample cluster script to run python libraries

--- ocr-benchmarking-on-sea-languages/cluster_scripts/hello_world.py
+++ ocr-benchmarking-on-sea-languages/cluster_scripts/hello_world.py
@@ -1,1 +1,9 @@
+import jiwer
+
 print('hello world!')
+
+target = 'hi'
+prediction = 'hello'
+
+print(jiwer.cer(target, prediction))
+print(jiwer.wer(target, prediction))
--- ocr-benchmarking-on-sea-languages/cluster_scripts/hello_world.sh
+++ ocr-benchmarking-on-sea-languages/cluster_scripts/hello_world.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --time=999
 #SBATCH --job-name=hello_world
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=jason.qiu@u.nus.edu
 
+source sample/bin/activate
 python3 hello_world.py
+deactivate
