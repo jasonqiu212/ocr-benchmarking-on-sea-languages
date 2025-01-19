dca533ea099584e26aefbde0f9980711467687a9
jasonqiu212 <jason_qiu@hotmail.com>
2025-01-19 23:17:14 +0800

    feat: split artificial documents into multiple pages

--- ocr-benchmarking-on-sea-languages/data_collection/generate.py
+++ ocr-benchmarking-on-sea-languages/data_collection/generate.py
@@ -26,38 +26,51 @@ def insert_character_at_nearest_space(text, char, n):
 
 
 def insert_character_every_n_chars(text, char, n):
-    # Insert `char` every `n` characters
     return char.join(text[i:i + n] for i in range(0, len(text), n))
 
 
-img_width, img_height = 600, 12000
-source = '../../data/vietnamese'
+img_width, img_height = 600, 1000
+source_path = '../../data/thai'
+destination_path = '../../artificial_data/thai'
 
 try:
-    font = ImageFont.truetype("BeVietnamPro.ttf", size=20)
+    font = ImageFont.truetype("NotoSansThai.ttf", size=20)
 except:
     font = ImageFont.load_default()
 
-for f in os.listdir(source):
+for f in os.listdir(source_path):
     print(f'Running on article: {f}')
 
-    text_file = f'{source}/{f}/text-clean.txt'
+    text_file = f'{destination_path}/{f}/text.txt'
     if not os.path.exists(text_file):
-        print(f'{f}: Clean does not exist')
+        print(f'{f}: Text does not exist')
         continue
 
-    text = open(text_file, 'r').read(20000)
+    text = open(text_file, 'r').read()
 
-    artificial_text_file = f'{source}/{f}/text-artificial.txt'
-    with open(artificial_text_file, 'w') as file:
-        file.write(text)
+    # artificial_text_file = f'{source}/{f}/text-artificial.txt'
+    # with open(artificial_text_file, 'w') as file:
+    #     file.write(text)
 
-    text = insert_character_at_nearest_space(text, '\n', 50)
-    # text = insert_character_every_n_chars(text, '\n', 50)
+    # text = insert_character_at_nearest_space(text, '\n', 50)
+    text = insert_character_every_n_chars(text, '\n', 50)
 
-    img = Image.new('RGB', (img_width, img_height), color=(255, 255, 255))
-    draw = ImageDraw.Draw(img)
+    lines = text.split('\n')
 
-    draw.multiline_text((20, 20), text, fill=(0, 0, 0), font=font, spacing=5)
+    lines = [s for s in lines if s]
 
-    img.save(f'{source}/{f}/artificial.png')
+    lines_per_page = 34
+    line_count = 0
+    page_count = 0
+    while line_count < len(lines):
+        lines_to_draw = '\n'.join(
+            lines[line_count:line_count + lines_per_page])
+        img = Image.new('RGB', (img_width, img_height), color=(255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        draw.multiline_text((20, 20), lines_to_draw, fill=(
+            0, 0, 0), font=font, spacing=5)
+        img.save(f'{destination_path}/{f}/page-{page_count}.png')
+
+        line_count += lines_per_page
+        page_count += 1
