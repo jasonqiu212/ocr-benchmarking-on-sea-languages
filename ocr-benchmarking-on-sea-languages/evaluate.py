23b05b41c1c951c3fcaacc3184f2b2e43b854c0e
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-03 18:30:32 +0800

    fix: fix unterminated string literal

--- ocr-benchmarking-on-sea-languages/evaluate.py
+++ ocr-benchmarking-on-sea-languages/evaluate.py
@@ -12,8 +12,7 @@ def evaluate_to_csv(source_path: str, target_file_name: str, prediction_file_nam
 
     for article in sorted_articles:
         target_file_path = f'{source_path}/{article}/{target_file_name}'
-        prediction_file_path = f'{
-            source_path}/{article}/{prediction_file_name}'
+        prediction_file_path = f'{source_path}/{article}/{prediction_file_name}'
 
         if os.path.exists(prediction_file_path) and os.path.exists(target_file_path):
             target_text = open(target_file_path, 'r').read()
