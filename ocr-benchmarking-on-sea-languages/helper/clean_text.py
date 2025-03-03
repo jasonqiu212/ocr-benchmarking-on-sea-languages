6ff12496e1e9d419f8a1cf100d6663406c5de377
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-03 18:23:23 +0800

    feat: add preprocessing of tesseract results v2

--- ocr-benchmarking-on-sea-languages/helper/clean_text.py
+++ ocr-benchmarking-on-sea-languages/helper/clean_text.py
@@ -80,3 +80,27 @@ def clean_tesseract(source_path: str, language: str):
 
 def clean_ground_truth(source_path: str):
     pass
+
+
+def clean_tesseract_v2(source_path: str):
+    for article in os.listdir(source_path):
+        dirty_file_path = f'{source_path}/{article}/tesseract-results.txt'
+        if not os.path.exists(dirty_file_path):
+            continue
+
+        clean_file_path = source_path + \
+            f'/{article}/tesseract-results-clean.txt'
+        if os.path.exists(clean_file_path):
+            continue
+
+        dirty_lines = open(dirty_file_path, 'r').readlines()
+        res = ''
+
+        for dirty_line in dirty_lines:
+            clean_line = dirty_line[1::2] if dirty_line[0] == ' ' else dirty_line[::2]
+            res += clean_line.strip() + ' '
+
+        res = res.strip()
+
+        with open(clean_file_path, 'wt') as outfile:
+            outfile.write(res)
