bcf59a67262614c6c6ea7d60c19fd847ea7ed583
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-11 16:40:12 +0800

    feat: add noise for links and headings

--- ocr-benchmarking-on-sea-languages/data_collection/generate.py
+++ ocr-benchmarking-on-sea-languages/data_collection/generate.py
@@ -9,8 +9,8 @@ from weasyprint import HTML
 class Noise(Enum):
     BOLD = 1
     ITALIC = 2
-    BLUE_COLOR = 3
-    UNDERLINE = 4
+    LINK = 3  # Blue and underline
+    HEADING = 4
 
 
 def generate_pdfs(source_path: str, font_path: str = None,
@@ -43,6 +43,7 @@ def generate_pdfs(source_path: str, font_path: str = None,
         text = open(text_file, 'r').read()
 
         if noise_type and noise_ratio:
+            # TODO: abstract out components into methods
             match noise_type:
                 case Noise.BOLD:
                     words = text.split()
@@ -50,20 +51,40 @@ def generate_pdfs(source_path: str, font_path: str = None,
                     bold_indices = set(random.sample(
                         range(len(words)), num_bold))
                     result = [
-                        f"<b>{word}</b>" if i in bold_indices else word
+                        f'<b>{word}</b>' if i in bold_indices else word
                         for i, word in enumerate(words)
                     ]
-                    text = " ".join(result)
+                    text = ' '.join(result)
                 case Noise.ITALIC:
                     words = text.split()
                     num_bold = int(len(words) * noise_ratio)
                     bold_indices = set(random.sample(
                         range(len(words)), num_bold))
                     result = [
-                        f"<i>{word}</i>" if i in bold_indices else word
+                        f'<i>{word}</i>' if i in bold_indices else word
                         for i, word in enumerate(words)
                     ]
-                    text = " ".join(result)
+                    text = ' '.join(result)
+                case Noise.LINK:
+                    words = text.split()
+                    num_bold = int(len(words) * noise_ratio)
+                    bold_indices = set(random.sample(
+                        range(len(words)), num_bold))
+                    result = [
+                        f'<a href="#">{word}</a>' if i in bold_indices else word
+                        for i, word in enumerate(words)
+                    ]
+                    text = ' '.join(result)
+                case Noise.HEADING:
+                    words = text.split()
+                    num_bold = int(len(words) * noise_ratio)
+                    bold_indices = set(random.sample(
+                        range(len(words)), num_bold))
+                    result = [
+                        f'<h1>{word}</h1>' if i in bold_indices else word
+                        for i, word in enumerate(words)
+                    ]
+                    text = ' '.join(result)
                 case _:
                     pass
 
@@ -74,3 +95,4 @@ def generate_pdfs(source_path: str, font_path: str = None,
 
         HTML(string=html_content, base_url='.').write_pdf(
             f'{source_path}/{f}/article.pdf')
+        break
