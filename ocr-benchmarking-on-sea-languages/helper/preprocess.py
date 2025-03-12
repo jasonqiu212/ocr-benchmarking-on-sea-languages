4bb82496798cc19f9eed5493048da3880aaae0dc
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-12 17:16:04 +0800

    feat: add tqdm for preprocess

--- ocr-benchmarking-on-sea-languages/helper/preprocess.py
+++ ocr-benchmarking-on-sea-languages/helper/preprocess.py
@@ -2,13 +2,14 @@ import os
 from typing import List, Tuple
 
 import pymupdf
+from tqdm import tqdm
 
 
 def convert_pdfs_to_pngs(source_path: str):
     """Converts and saves all PDFs into PNGs."""
     zoom_x = zoom_y = 2.0
     mat = pymupdf.Matrix(zoom_x, zoom_y)
-    for f in os.listdir(source_path):
+    for f in tqdm(os.listdir(source_path)):
         path = f'{source_path}/{f}'
         pdf = pymupdf.open(f'{path}/article.pdf')
         for page in pdf:
