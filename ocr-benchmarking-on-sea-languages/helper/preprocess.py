25410d47481f7289d72909be84c01ae356ad5a1b
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-18 14:39:49 +0800

    feat: add util function to count article pages

--- ocr-benchmarking-on-sea-languages/helper/preprocess.py
+++ ocr-benchmarking-on-sea-languages/helper/preprocess.py
@@ -1,5 +1,4 @@
 import os
-from typing import List, Tuple
 
 import pymupdf
 from tqdm import tqdm
@@ -15,26 +14,3 @@ def convert_pdfs_to_pngs(source_path: str):
         for page in pdf:
             pix = page.get_pixmap(matrix=mat)
             pix.save(f'{path}/page-%i.png' % page.number)
-
-
-def get_articles_sorted_by_length(source_path: str) -> List[Tuple[str, int]]:
-    """
-    Get articles sorted by length.
-
-    Args:
-        source_path: Path to get all articles
-
-    Returns:
-        List of articles in source path ranked in increasing lengths
-    """
-    res = []
-
-    for f in os.listdir(source_path):
-        i = 0
-        image_file_path = f'{source_path}/{f}/page-{i}.png'
-        while os.path.exists(image_file_path):
-            i += 1
-            image_file_path = f'{source_path}/{f}/page-{i}.png'
-        res.append((f, i))
-
-    return sorted(res, key=lambda x: x[1])
---
+++ ocr-benchmarking-on-sea-languages/helper/utils.py
@@ -0,0 +1,40 @@
+from typing import List, Tuple
+
+
+def get_articles_sorted_by_length(source_path: str) -> List[Tuple[str, int]]:
+    """
+    Get articles sorted by length.
+
+    Args:
+        source_path: Path to get all articles
+
+    Returns:
+        List of articles in source path ranked in increasing lengths
+    """
+    res = []
+
+    for f in os.listdir(source_path):
+        i = 0
+        image_file_path = f'{source_path}/{f}/page-{i}.png'
+        while os.path.exists(image_file_path):
+            i += 1
+            image_file_path = f'{source_path}/{f}/page-{i}.png'
+        res.append((f, i))
+
+    return sorted(res, key=lambda x: x[1])
+
+
+def count_article_pages(source_path: str):
+    res = []
+    for article in os.listdir(source_path):
+        i = 0
+        image_file_path = f'{source_path}/{article}/page-{i}.png'
+        if not os.path.exists(image_file_path):
+            continue
+
+        while os.path.exists(image_file_path):
+            i += 1
+            image_file_path = f'{source_path}/{article}/page-{i}.png'
+
+        res.append((article, i))
+    return sorted(res)
