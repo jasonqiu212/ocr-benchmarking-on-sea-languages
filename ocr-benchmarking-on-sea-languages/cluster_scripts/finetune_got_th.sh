66453f403b2a5116fb168f3d1b7f1f07df6a5af1
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-19 00:09:19 +0800

    fix: use 1 gpu

--- ocr-benchmarking-on-sea-languages/cluster_scripts/finetune_got_th.sh
+++ ocr-benchmarking-on-sea-languages/cluster_scripts/finetune_got_th.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 #SBATCH --time=7199
 #SBATCH --job-name=finetune_got_th
-#SBATCH --gpus=4
+#SBATCH --gpus=1
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jason.qiu@u.nus.edu
 
 source sample/bin/activate
 
-NPROC_PER_NODE=4 \
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
-swift sft \
-    --model_type got-ocr2 \
-    --model_id_or_path stepfun-ai/GOT-OCR2_0 \
-    --sft_type lora \
-    --dataset ../finetuning/thai-dataset.json \
-    --deepspeed default-zero2
+CUDA_VISIBLE_DEVICES=0 swift sft \
+    --model stepfun-ai/GOT-OCR2_0 \
+    --dataset ../finetuning/thai-dataset.json
 
 deactivate
