b64bc31d231b629ef015113c84b0645d1e9e3ce0
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-19 13:27:44 +0800

    feat: add inference script for vietnamese

--- ocr-benchmarking-on-sea-languages/cluster_scripts/infer_finetuned_got_vi.sh
+++ ocr-benchmarking-on-sea-languages/cluster_scripts/infer_finetuned_got_vi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=7199
-#SBATCH --job-name=infer_finetuned_got_th
+#SBATCH --job-name=infer_finetuned_got_vi
 #SBATCH --gpus=1
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jason.qiu@u.nus.edu
@@ -8,7 +8,7 @@
 source sample/bin/activate
 
 CUDA_VISIBLE_DEVICES=0 swift infer \
-    --adapters output/GOT-OCR2_0/v0-20250319-105427/checkpoint-864 \
+    --adapters output/GOT-OCR2_0/v0-20250319-105430/checkpoint-1212 \
     --infer_backend pt \
     --max_batch_size 1 \
     --load_data_args true
