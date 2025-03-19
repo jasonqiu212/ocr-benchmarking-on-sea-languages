0aaeaee50859262bf616e80a39f334d1d1e22faf
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-19 10:51:51 +0800

    fix: change model to adapter

--- ocr-benchmarking-on-sea-languages/cluster_scripts/infer_finetuned_got_th.sh
+++ ocr-benchmarking-on-sea-languages/cluster_scripts/infer_finetuned_got_th.sh
@@ -8,7 +8,7 @@
 source sample/bin/activate
 
 CUDA_VISIBLE_DEVICES=0 swift infer \
-    --model output/GOT-OCR2_0/v1-20250318-235123/checkpoint-1071 \
+    --adapters output/GOT-OCR2_0/v1-20250318-235123/checkpoint-1071 \
     --infer_backend pt \
     --max_batch_size 1 \
     --load_data_args true
