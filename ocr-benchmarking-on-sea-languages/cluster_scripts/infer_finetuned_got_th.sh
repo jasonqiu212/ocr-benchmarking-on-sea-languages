77c5d49e5bcfaa66d9766db6f4ff4fc797ac75d9
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-19 10:42:35 +0800

    fix: change to batch processing

--- ocr-benchmarking-on-sea-languages/cluster_scripts/infer_finetuned_got_th.sh
+++ ocr-benchmarking-on-sea-languages/cluster_scripts/infer_finetuned_got_th.sh
@@ -8,7 +8,9 @@
 source sample/bin/activate
 
 CUDA_VISIBLE_DEVICES=0 swift infer \
-    --ckpt_dir output/GOT-OCR2_0/v1-20250318-235123/checkpoint-1071 \
-    --load_dataset_config true \
+    --model output/GOT-OCR2_0/v1-20250318-235123/checkpoint-1071 \
+    --infer_backend pt \
+    --max_batch_size 1 \
+    --load_data_args true
 
 deactivate
