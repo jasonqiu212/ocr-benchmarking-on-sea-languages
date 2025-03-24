565509228e94b567d8aae81e60245020fa530177
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-24 17:09:15 +0800

    feat: prepare dataset for problematic articles

--- ocr-benchmarking-on-sea-languages/finetuning/prepare_dataset.py
+++ ocr-benchmarking-on-sea-languages/finetuning/prepare_dataset.py
@@ -24,3 +24,29 @@ def prepare_dataset(source_path: str, output_file_name: str):
 
     with open(f'{output_file_name}.json', 'w') as output_file:
         json.dump(dataset, output_file, ensure_ascii=False)
+
+
+def prepare_dataset_v2(source_path: str, output_file_name: str):
+    problems = ['Adolf_Hitler/page-2.png', 'Adolf_Hitler/page-7.png', 'Canada/page-7.png', 'Eminem/page-5.png', 'Game_of_Thrones/page-1.png', 'Japan/page-1.png', 'Jerusalem/page-4.png', 'Johnny_Depp/page-7.png', 'Justin_Bieber/page-5.png',
+                'Lionel_Messi/page-8.png', 'Machu_Picchu/page-3.png', 'Michael_Jackson/page-1.png', 'Spain/page-6.png', 'Steve_Jobs/page-8.png', 'United_Kingdom/page-0.png', 'United_States/page-6.png', 'Washington,_D.C./page-8.png']
+    dataset = []
+
+    for problem in problems:
+        problem_no_ext = problem.replace('.png', '')
+        number = int(problem_no_ext.split('-')[-1])
+        article = problem_no_ext.split('/')[0]
+
+        text_file_path = f'{source_path}/{article}/page-{number}-text.txt'
+        text = open(text_file_path, 'r').read()
+
+        image_path = f'{source_path}/{article}/page-{number}.png'
+        data_entry = {
+            'query': 'OCR <image>',
+            'response': text,
+            'images': [
+                image_path
+            ]}
+        dataset.append(data_entry)
+
+    with open(f'{output_file_name}.json', 'w') as output_file:
+        json.dump(dataset, output_file, ensure_ascii=False)
