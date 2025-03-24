299a175ab546e6cce1fcc7e705600a0def65f2dc
jasonqiu212 <jason_qiu@hotmail.com>
2025-03-24 22:48:23 +0800

    feat: add evaluation of error by character class

--- ocr-benchmarking-on-sea-languages/evaluate.py
+++ ocr-benchmarking-on-sea-languages/evaluate.py
@@ -1,10 +1,13 @@
 """This file provides operations to evaluate OCR performance."""
 
 import csv
+from collections import Counter
 import json
 import os
+import re
 
 import jiwer
+from Levenshtein import editops
 from tqdm import tqdm
 
 
@@ -56,6 +59,75 @@ def evaluate_finetuned(results_file_path: str, output_file_name: str):
         writer.writerows(data)
 
 
-# def evaluate(source_path: str, target_file_name: str, prediction_file_name: str, output_file_name: str):
-    #
-    # pass
+def evaluate_error_by_character_class(source_path: str, correct_file_name: str, predicted_file_name: str, output_file_name: str):
+    data = ['Article Name', 'arabic_digit', 'thai_digit', 'latin_letter', 'thai_letter', 'vietnamese_letter_with_diacritic',
+            'thai_diacritic', 'punctuation', 'thai_punctuation', 'vietnamese_punctuation', 'whitespace', 'other']
+    sorted_articles = sorted(os.listdir(source_path))
+
+    for article in tqdm(sorted_articles):
+        correct_file_path = f'{source_path}/{article}/{correct_file_name}'
+        if not os.path.exists(correct_file_path):
+            print(f'{article}: {correct_file_path} does not exist.')
+            continue
+
+        predicted_file_path = f'{source_path}/{article}/{predicted_file_name}'
+        if not os.path.exists(predicted_file_path):
+            print(f'{article}: {predicted_file_path} does not exist.')
+            continue
+
+        error_counts = Counter()
+
+        correct = open(correct_file_path, 'r').read()
+        predicted = open(predicted_file_path, 'r').read()
+        operations = editops(correct, predicted)
+
+        for operation, i, j in operations:
+            if operation == 'replace':  # Substitution
+                char_class = _classify_char(correct[i])
+                error_counts[char_class] += 1
+            elif operation == 'insert':  # Insertion in predicted
+                char_class = _classify_char(predicted[j])
+                error_counts[char_class] += 1
+            elif operation == 'delete':  # Deletion from target
+                char_class = _classify_char(correct[i])
+                error_counts[char_class] += 1
+
+        data.append([article, error_counts['arabic_digit'], error_counts['thai_digit'],
+                     error_counts['latin_letter'], error_counts['thai_letter'],
+                     error_counts['vietnamese_letter_with_diacritic'],
+                    error_counts['thai_diacritic'], error_counts['punctuation'],
+                    error_counts['thai_punctuation'], error_counts['vietnamese_punctuation'],
+                    error_counts['whitespace'], error_counts['other']])
+
+        with open(f'{source_path}/{output_file_name}.csv', 'w', newline='') as output_file:
+            writer = csv.writer(output_file)
+        writer.writerows(data)
+
+
+def _classify_char(char: str):
+    if char.isdigit():
+        return "arabic_digit"
+    if '๐' <= char <= '๙':
+        return "thai_digit"
+
+    elif 'A' <= char <= 'Z' or 'a' <= char <= 'z':
+        return "latin_letter"
+    elif 'ก' <= char <= 'ฮ':
+        return "thai_letter"
+    elif re.match(r"[àáảãạâầấẩẫậăằắẳẵặèéẻẽẹêềếểễệìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳýỷỹỵ]", char):
+        return "vietnamese_letter_with_diacritic"
+    elif re.match(r"[่้๊๋็์]", char):
+        return "thai_diacritic"
+
+    elif re.match(r"[.,!?;:()\-\"']", char):
+        return "punctuation"
+    elif re.match(r"[ๆฯ]", char):
+        return "thai_punctuation"
+    elif re.match(r"[«»]", char):
+        return "vietnamese_punctuation"
+
+    elif char.isspace():  # Whitespace
+        return "whitespace"
+
+    else:
+        return "other"
