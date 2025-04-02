7b16a68547bd99348d6918568487f3ce1995988c
jasonqiu212 <jason_qiu@hotmail.com>
2025-04-02 17:01:20 +0800

    feat: add more dataset preparation for finetuning

--- ocr-benchmarking-on-sea-languages/finetuning/prepare_dataset.py
+++ ocr-benchmarking-on-sea-languages/finetuning/prepare_dataset.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 
 def prepare_dataset(source_path: str, output_file_name: str):
@@ -25,42 +26,29 @@ def prepare_dataset(source_path: str, output_file_name: str):
     with open(f'{output_file_name}.json', 'w') as output_file:
         json.dump(dataset, output_file, ensure_ascii=False)
 
-# Good in vi-1200, but bad in vi-500
-# Japan/page-1.png
-# Michael_Jackson/page-1.png
 
-# Good in vi-500, but bad in vi-1200
-# India/page-7.png
+def train_test_split_from_dataset(source_path: str, dataset_file_name: str, num_of_test_samples: float):
+    with open(f'{source_path}/{dataset_file_name}', 'r') as dataset_file:
+        data = json.load(dataset_file)
 
-# no pages that are bad for both vi-500 and vi-1200
+        random.shuffle(data)
+        test_dataset = data[:num_of_test_samples]
+        train_dataset = data[num_of_test_samples:]
+
+        with open(f'{source_path}/train.json', 'w') as train_dataset_file:
+            json.dump(train_dataset, train_dataset_file, ensure_ascii=False)
+
+        with open(f'{source_path}/test.json', 'w') as test_dataset_file:
+            json.dump(test_dataset, test_dataset_file, ensure_ascii=False)
 
 
-def prepare_dataset_v2(source_path: str, output_file_name: str):
-    problems = ['Adolf_Hitler/page-2.png', 'Canada/page-7.png', 'Eminem/page-5.png', 'Japan/page-1.png',
-                'Lionel_Messi/page-8.png', 'Machu_Picchu/page-3.png', 'Spain/page-6.png', 'Steve_Jobs/page-8.png', 'United_Kingdom/page-7.png', 'Washington,_D.C./page-8.png']
-    # problems = ['Andromeda_Galaxy/page-2.png', 'Australia/page-4.png', 'Big_Bang/page-1.png', 'Big_Bang/page-11.png', 'Big_Bang/page-15.png', 'Big_Bang/page-18.png', 'Big_Bang/page-20.png', 'Cat/page-3.png', 'Donald_Trump/page-0.png', 'Donald_Trump/page-4.png', 'Eiffel_Tower/page-6.png', 'Elon_Musk/page-9.png', 'Germany/page-4.png', 'Germany/page-7.png', 'Golden_Gate_Bridge/page-1.png', 'India/page-7.png', 'India/page-9.png', 'Israel/page-8.png', 'Jerusalem/page-3.png', 'Jupiter/page-12.png', 'Jupiter/page-5.png', 'Justin_Bieber/page-0.png',
-    #             'Machu_Picchu/page-0.png', 'Moon/page-1.png', 'Moon/page-7.png', 'Neptune/page-3.png', 'Neptune/page-6.png', 'New Zealand/page-6.png', 'Pakistan/page-0.png', 'Pakistan/page-1.png', 'Panama_Canal/page-6.png', 'Philippines/page-4.png', 'Pluto/page-19.png', 'Potato/page-6.png', 'San_Francisco/page-2.png', 'San_Francisco/page-5.png', 'Spain/page-5.png', 'Stephen_Hawking/page-8.png', 'Sun/page-11.png', 'Sun/page-22.png', 'Sun/page-4.png', 'Sun/page-6.png', 'Toronto/page-2.png', 'United_Kingdom/page-8.png', 'Universe/page-15.png', 'Venus/page-22.png']
-    # problems = ['Italy/page-7.png', 'Galaxy/page-4.png',
-    #             'Cambodia/page-7.png', 'Big_Bang/page-9.png', 'Bangkok/page-7.png']
+def select_samples_from_dataset(source_path: str, dataset_file_name: str, num_of_samples: str):
+    with open(f'{source_path}/{dataset_file_name}', 'r') as dataset_file:
+        data = json.load(dataset_file)
 
-    dataset = []
+        random.shuffle(data)
+        selected_dataset = data[:num_of_samples]
 
-    for problem in problems:
-        problem_no_ext = problem.replace('.png', '')
-        number = int(problem_no_ext.split('-')[-1])
-        article = problem_no_ext.split('/')[0]
-
-        text_file_path = f'{source_path}/{article}/page-{number}-text.txt'
-        text = open(text_file_path, 'r').read()
-
-        image_path = f'{source_path}/{article}/page-{number}.png'
-        data_entry = {
-            'query': 'OCR <image>',
-            'response': text,
-            'images': [
-                image_path
-            ]}
-        dataset.append(data_entry)
-
-    with open(f'{output_file_name}.json', 'w') as output_file:
-        json.dump(dataset, output_file, ensure_ascii=False)
+        with open(f'{source_path}/{num_of_samples}.json', 'w') as selected_dataset_file:
+            json.dump(selected_dataset, selected_dataset_file,
+                      ensure_ascii=False)
